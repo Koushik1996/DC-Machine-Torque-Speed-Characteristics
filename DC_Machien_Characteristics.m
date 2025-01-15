363890c8602dcce471223fe5ba21695fa030e577
Koushik <koushik96shenoy@gmail.com>
2025-01-15 11:21:37 +0100

    Making it more user friendly by adding input commands

--- DC_Machien_Characteristics.m
+++ DC_Machien_Characteristics.m
@@ -3,19 +3,22 @@ close all;
 clc;
 
 %% Parameter Assignment
-R = 0.5;
-L = 0.003;
-Ke = 0.8;
-Ua = 220;
-J = 0.0167;
-bm = 0.01;
+R = input("Armature Resistance (in ohm) = "); %0.5;
+L = input("Armature Inductance (in H) = "); %0.003;
+Ke = input("Motor Constant (in V/(rad/sec)) = "); %0.8;
+Ua = input("Supply Voltage (in V) = "); %220;
+J = input("Motor Inertia (in kg m^2) = "); %0.0167;
+bm = input("Friction Coefficient (in Nm / (rad/sec)) = "); %0.01;
 
 %% Running simulation
 Data = sim("DC_Machine_Characteristics.slx");
 
+%% Unit conversion from rad/sec to RPM
+RPM = Data.Speed * (60 / (2 * pi));
+
 %% plotting
 figure(1)
-plot(Data.Speed, Data.Torque)
+plot(RPM, Data.Torque)
 xlabel("Speed (RPM)")
 ylabel("Torque (Nm)")
 grid on
